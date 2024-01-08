d30e346737089d138f87ab167341bfac50faaa53
aviip <avipatel.tech123@gmail.com>
2024-01-08 11:30:33 +0530

    The Auth Form is in progress

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/input";
 import { useCallback, useState } from "react";
 import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -29,7 +32,7 @@ const AuthForm = () => {
     },
   });
 
-  const onSubmit: SubmitHandler<FieldValues> = () => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
     if (variant === "REGISTER") {
       // axios register
@@ -39,7 +42,63 @@ const AuthForm = () => {
     }
   };
 
-  return <div></div>;
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+    // nextAuth social sign in
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {/* Here we need to wrap the onSubmit function into handleSubmit because we want the data of the form*/}
+          {variant === "REGISTER" && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+
+          <Input
+            id="email"
+            label="Email Address"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign In" : "Register"}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
